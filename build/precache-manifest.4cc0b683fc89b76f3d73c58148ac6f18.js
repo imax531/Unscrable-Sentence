077c7516b31cdf6cc72a6b7f887461a1d69f9e27
Max Roytman <imax531@gmail.com>
2020-04-30 23:05:46 +0300

    pages fix attempt #3

---
+++ build/precache-manifest.4cc0b683fc89b76f3d73c58148ac6f18.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11ceb24deff33ab477e9a7520c3bdc43",
+    "url": "/unscrable-sentence/build/index.html"
+  },
+  {
+    "revision": "fd87a566c049e80dd4fb",
+    "url": "/unscrable-sentence/build/static/css/main.82790ce8.chunk.css"
+  },
+  {
+    "revision": "eaa649aba548606f1d38",
+    "url": "/unscrable-sentence/build/static/js/2.4907657a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/unscrable-sentence/build/static/js/2.4907657a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd87a566c049e80dd4fb",
+    "url": "/unscrable-sentence/build/static/js/main.8742e873.chunk.js"
+  },
+  {
+    "revision": "04660401be0dffcc2874",
+    "url": "/unscrable-sentence/build/static/js/runtime-main.ed178000.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/build/precache-manifest.188efdf4981abfcff6f16c639a47ce3b.js"
+  "/unscrable-sentence/build/precache-manifest.4cc0b683fc89b76f3d73c58148ac6f18.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/build/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/unscrable-sentence/build/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
